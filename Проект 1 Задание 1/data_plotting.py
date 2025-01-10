eabc43f8c90b61c809178a029ee0b52e3b86c102
AVG-Miami <166794127+AVG-Miami@users.noreply.github.com>
2025-01-10 13:05:56 +0800

    Update data_plotting.py

--- Проект 1 Задание 1/data_plotting.py
+++ Проект 1 Задание 1/data_plotting.py
@@ -1,34 +1,40 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from openpyxl import load_workbook
+from openpyxl.drawing.image import Image
+from openpyxl.chart import BarChart, Reference
+import os.path
+from time import sleep
 
 
 def create_and_save_plot(data, ticker, period, filename=None):
-    plt.figure(figsize=(10, 6))
+    plt.figure( figsize=(10, 6) )
 
     if 'Date' not in data:
-        if pd.api.types.is_datetime64_any_dtype(data.index):
+        if pd.api.types.is_datetime64_any_dtype( data.index ):
             dates = data.index.to_numpy()
-            plt.plot(dates, data['Close'].values, label='Close Price')
-            plt.plot(dates, data['Moving_Average'].values, label='Moving Average')
+            plt.plot( dates, data['Close'].values, label='Close Price' )
+            plt.plot( dates, data['Moving_Average'].values, label='Moving Average' )
         else:
-            print("Информация о дате отсутствует или не имеет распознаваемого формата.")
+            print( "Информация о дате отсутствует или не имеет распознаваемого формата." )
             return
     else:
-        if not pd.api.types.is_datetime64_any_dtype(data['Date']):
-            data['Date'] = pd.to_datetime(data['Date'])
-        plt.plot(data['Date'], data['Close'], label='Close Price')
-        plt.plot(data['Date'], data['Moving_Average'], label='Moving Average')
+        if not pd.api.types.is_datetime64_any_dtype( data['Date'] ):
+            data['Date'] = pd.to_datetime( data['Date'] )
+        plt.plot( data['Date'], data['Close'], label='Close Price' )
+        plt.plot( data['Date'], data['Moving_Average'], label='Moving Average' )
 
-    plt.title(f"{ticker} Цена акций с течением времени")
-    plt.xlabel("Дата")
-    plt.ylabel("Цена")
+    plt.title( f"{ticker} Цена акций с течением времени" )
+    plt.xlabel( "Дата" )
+    plt.ylabel( "Цена" )
     plt.legend()
 
     if filename is None:
         filename = f"{ticker}_{period}_stock_price_chart.png"
 
-    plt.savefig(filename)
-    print(f"График сохранен как {filename}")
+    plt.savefig( filename )
+    print( f"График сохранен как {filename}" )
+    return filename
 
 
 def export_data_to_exel(data, tikers, graphic):
