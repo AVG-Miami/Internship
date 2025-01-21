a9ee15d925db96f3bbf8f957b06a47e3f1deb250
AVG-Miami <166794127+AVG-Miami@users.noreply.github.com>
2025-01-21 10:34:04 +0800

    Update data_download.py

--- Проект 1 Задание 1/data_download.py
+++ Проект 1 Задание 1/data_download.py
@@ -5,39 +5,39 @@ import yfinance as yf
 # import pandas_ta as pta
 
 
-def fetch_stock_data(ticker, period, start_date, end_date, interval="1d"):
-    stock = yf.Ticker( ticker )
+def fetch_stock_data(ticker, period, start_date, end_date, my_interval="1d"):
+    stock = yf.Ticker(ticker)
     if period != '':
-        data = stock.history( period=period )
+        data = stock.history(period=period)
     else:
-        data = stock.history( start=start_date, end=end_date, interval=interval )
+        data = stock.history(start=start_date, end=end_date, interval=my_interval)
     return data
 
 
 def add_moving_average(data, window_size=5):
-    data['Moving_Average'] = data['Close'].rolling( window=window_size ).mean()
+    data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
     return data
 
 
 def calculate_and_display_average_price(data):
     """ Вывод средней цены за период. """
-    if len( data['Close'] ) > 0:
-        average = sum( data['Close'] ) / len( data['Close'] )
+    if len(data['Close']) > 0:
+        average = sum(data['Close']) / len(data['Close'])
         print(
-            f" Сумма значений за период = {sum( data['Close'] )}, количество значений= {len( data['Close'] )}, среднее "
-            f'значение = {average}' )
+            f" Сумма значений за период = {sum(data['Close'])}, количество значений= {len(data['Close'])}, среднее "
+            f'значение = {average}')
     else:
-        print( 'Нет данных' )
+        print('Нет данных')
 
 
 def notify_if_strong_fluctuations(data, threshold):
     """ Функция уведомление о сильных колебаниях. """
-    maximum = max( data['Close'] )
-    minimum = min( data['Close'] )
-    difference = round( (maximum - minimum) / minimum * 100, 2 )
-    if difference > int( threshold ):
+    maximum = max(data['Close'])
+    minimum = min(data['Close'])
+    difference = round((maximum - minimum) / minimum * 100, 2)
+    if difference > int(threshold):
         print(
-            f" Разница между max и min ценой составляет {difference} % от min цены, что превышает заданный порг в {threshold} %" )
+            f" Разница между max и min ценой составляет {difference} % от min цены, что превышает заданный порг в {threshold} %")
 
 
 def add_rsi_calculate(data, periods=5):
@@ -45,11 +45,11 @@ def add_rsi_calculate(data, periods=5):
 
     close_delta = data['Close'].diff()
     # Делаем две серий: одну для низких закрытий и одну для высоких закрытий
-    up = close_delta.clip( lower=0 )
-    down = -1 * close_delta.clip( upper=0 )
+    up = close_delta.clip(lower=0)
+    down = -1 * close_delta.clip(upper=0)
     # Используем простую скользящую среднюю
-    ma_up = up.rolling( window=periods ).mean()
-    ma_down = down.rolling( window=periods ).mean()
+    ma_up = up.rolling(window=periods).mean()
+    ma_down = down.rolling(window=periods).mean()
     rsi = ma_up / ma_down
     rsi = 100 - (100 / (1 + rsi))
     data['RSI'] = rsi
@@ -59,15 +59,15 @@ def add_macd_calculate(data, fast=None, slow=None, signal=None, offset=None, **k
     """ Добавление MACD в dataset  """
 
     # Получаем 26-дневную EMA цены закрытия
-    k = data['Close'].ewm( span=12, adjust=False, min_periods=12 ).mean()
+    k = data['Close'].ewm(span=12, adjust=False, min_periods=12).mean()
     # Получаем 12-дневную EMA цены закрытия
-    d = data['Close'].ewm( span=26, adjust=False, min_periods=26 ).mean()
+    d = data['Close'].ewm(span=26, adjust=False, min_periods=26).mean()
     # Вычитаем 26-дневную EMA из 12-дневной EMA, чтобы получить MACD
     macd = k - d
-    data['MACD'] = data.index.map( macd )
+    data['MACD'] = data.index.map(macd)
     # Получаем 9-дневную EMA MACD для линии срабатывания
-    macd_s = macd.ewm( span=9, adjust=False, min_periods=9 ).mean()
+    macd_s = macd.ewm(span=9, adjust=False, min_periods=9).mean()
     # Рассчитаем разницу между MACD - Триггером для значения Конвергенции/Дивергенции
     macd_h = macd - macd_s
-    data['MACD_H'] = data.index.map( macd_h )
-    data['MACD_S'] = data.index.map( macd_s )
+    data['MACD_H'] = data.index.map(macd_h)
+    data['MACD_S'] = data.index.map(macd_s)
